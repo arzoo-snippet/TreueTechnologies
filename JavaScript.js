7d87ade05e3105b20cd497962d9701b1a476849e
arzoo-snippet <141822937+arzoo-snippet@users.noreply.github.com>
2023-08-09 15:16:11 +0530

    images/coffeee

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.querySelector(".slider");
+    const images = document.querySelectorAll(".slider img");
+
+    let currentIndex = 0;
+    const totalImages = images.length;
+    const slideWidth = images[0].clientWidth;
+
+    function slideNext() {
+        currentIndex = (currentIndex + 1) % totalImages;
+        slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    }
+
+    setInterval(slideNext, 3000); // Change slide every 3 seconds (adjust as needed)
+});
